dc741c827ad33c845fe3058a039323ac8eb1bc2a
Giacomo Bonaccorsi <gbonaccorsi@eng.ucsd.edu>
2021-07-25 21:42:25 -0700

    Started implementing quick-find client test

--- tests/union_find_integration_tests.cpp
+++ tests/union_find_integration_tests.cpp
@@ -1,8 +1,35 @@
 #include "doctest.h"
 #include "algorithms/union_find.h"
+#include "algorithms/quick_find.h"
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+
+void connectAccordingToFile(UnionFind* uf, std::ifstream &file);
 
 TEST_CASE("Dynamic-connectivity client")
 {
-//   Dummy d;
-//    CHECK(true == true);
+    std::ifstream client_file("union_find_client_file.txt");
+    int n_elements;
+    client_file >> n_elements;
+    
+    SUBCASE("Quick-find algorithm")
+    {
+        UnionFind *union_find = new QuickFind(n_elements);
+        connectAccordingToFile(union_find, client_file);
+    }
+}
+
+void connectAccordingToFile(UnionFind* uf, std::ifstream &file)
+{
+    int p, q;
+    while (file >> p >> q)
+    {
+        if (!uf->connected(p, q))
+        {
+            uf->join(p, q);
+            std::cout << p << ' ' << q << '\n';
+        }
+    }
 }
